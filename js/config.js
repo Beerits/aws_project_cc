b6324c775cbb47fc9587d9a5a8102f72e88a9f1d
ilikecatsalot <111728441+ilikecatsalot@users.noreply.github.com>
2024-12-03 03:23:51 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_KZlZhZ42d', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3kdlmgn49mpid3jsich1425v', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Cg0ISEiXS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1t6bt332gqlvuk432lne7o682', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
