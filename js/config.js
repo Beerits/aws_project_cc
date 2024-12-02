07d185189bafe9398ff76ce9c4c403765257046a
Ritisha Deka <128310447+Beerits@users.noreply.github.com>
2024-12-03 03:41:01 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_ugChZ2Tqd', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '616nmkk8sq5iern97jrl62puu0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_EbAqdMj6e', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1n9vk7ms4atdq94ceq877mihpl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
